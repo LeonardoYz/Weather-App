f5bae968f088de500b846392d6ca6bfdfeb221bf
Leonardo Yzquierdo <leoyzquierdo@gmail.com>
2022-05-22 14:05:48 -0300

    Rename the api.ts file to weatherApi.ts and change the base url to the new api

--- src/services/api.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const CROSS_DOMAIN = "https://mycorsproxy-crossdomainyz.herokuapp.com/"
-const API_URL = "https://www.metaweather.com/api/location/"
-
-export const api = axios.create({
-  baseURL: `${CROSS_DOMAIN}${API_URL}`,
-})
