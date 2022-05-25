0807b81742a429090a476b460ab862bbabef0bd5
Leonardo Yzquierdo <leoyzquierdo@gmail.com>
2022-05-24 21:45:34 -0300

    Change api variable name to weatherApi

--- src/services/weatherApiBaseUrl.ts
+++ src/services/weatherApiBaseUrl.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 
 const WEATHER_API_URL = "https://api.weatherbit.io/v2.0/forecast/daily?"
 
-export const api = axios.create({
+export const weatherApi = axios.create({
   baseURL: `${WEATHER_API_URL}`,
 })
